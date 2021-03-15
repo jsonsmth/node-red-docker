ce2f2558b1b5a607ca7bf5af6ba52336b0b2bcb8
Jason S <jason@staypath.com>
2021-03-15 10:07:54 -0400

    Update Dockerfile.cloudron - add gosu

--- docker-custom/start.sh
+++ docker-custom/start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+set -eu
+
 if [[ ! -d /app/data/default ]]
 then
     # this is first install, so setup /app/data and initial settings
     mkdir -p /app/data/default
-    chown -R node-red:root /app/data && chmod -R g+rwX /app/data
     cp /app/code/node-red/settings.js /app/data
     cp /app/code/node-red/flows.json /app/data
     cp index.html /app/data/default
+    chown -R node-red:root /app/data && chmod -R g+rwX /app/data
 fi
 
 echo "Starting Node-Red"
-exec /usr/local/bin/gosu node-red:root npm start --cache /app/data/.npm --userDir /app/data
+
+exec gosu node-red:root npm start --cache /app/data/.npm --userDir /app/data
