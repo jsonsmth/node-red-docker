863b5b92eedfd733d34268699cd497c47aa9946a
Jason S <jason@staypath.com>
2021-03-15 09:56:49 -0400

    Update start.sh for typo

--- docker-custom/start.sh
+++ docker-custom/start.sh
@@ -1,4 +1,7 @@
-if [[ ! -d /app/data/default ]]; then
+#!/bin/bash
+
+if [[ ! -d /app/data/default ]]
+then
     # this is first install, so setup /app/data and initial settings
     mkdir -p /app/data/default
     chown -R node-red:root /app/data && chmod -R g+rwX /app/data
